7a2ccf709c6e14c736f60320c11bb10b21e6983f
zcc <892212481@qq.com>
2024-08-23 19:17:38 +0800

    feat: 新增array-last

--- packages/array-last/src/index.ts
+++ packages/array-last/src/index.ts
@@ -1,15 +1,15 @@
 
-function arrayFirst<T>(arr: T[], n: number = 1): T[] | T {
+
+function arrayLast<T>(arr: T[], n: number = 1): T[] | T {
     const len = arr.length;
     if (len === 0) {
         throw new Error('arrayFirst: array is empty');
     }
-
     if (len < n) {
         throw new Error('arrayFirst: n is greater than array length');
     }
 
-    return n === 1? arr[0]: arr.slice(0, n);
+    return n === 1? arr[len - 1]: arr.slice(n, len);
 }
 
-export default arrayFirst;
+export default arrayLast;
--- packages/array-last/src/test/index.test.ts
+++ packages/array-last/src/test/index.test.ts
@@ -1,24 +1,24 @@
-import arrayFirst from '../index';
+import arrayLast from '../index';
 
 describe('first', function() {
     it('should throw an error if the value passed is not an array:', function() {
-      expect(() => arrayFirst([])).toThrow();
+      expect(() => arrayLast([])).toThrow();
     });
 
     it('should throw an error if the value passed is not an array:', function() {
-        expect(arrayFirst).toThrow();
+        expect(arrayLast).toThrow();
       });
   
     it('should return the first element in the array:', function() {
-       expect(arrayFirst(['a', 'b', 'c', 'd', 'e', 'f'])).toEqual('a');
-       expect(arrayFirst(['b', 'b', 'c', 'd', 'e', 'f'])).toEqual('b');
+       expect(arrayLast(['a', 'b', 'c', 'd', 'e', 'f'])).toEqual('f');
+       expect(arrayLast(['b', 'b', 'c', 'd', 'e', 'e'])).toEqual('e');
     });
   
     it('should the first n elements of the array:', function() {
-       expect(arrayFirst(['a', 'b', 'c', 'd', 'e', 'f'], 3)).toEqual(['a', 'b', 'c']);
+       expect(arrayLast(['a', 'b', 'c', 'd', 'e', 'f'], 3)).toEqual(['d', 'e', 'f']);
     });
 
     it('should the first n elements of the array:', function() {
-        expect(() => arrayFirst(['a', 'b'], 3)).toThrow();
+        expect(() => arrayLast(['a', 'b'], 3)).toThrow();
      });
   });
